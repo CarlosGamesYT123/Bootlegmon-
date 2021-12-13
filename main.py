059ebe47df1fb8c5c28be5b029581869584369e5
Carlos Cervantes <95884186+CarlosGamesYT123@users.noreply.github.com>
2021-12-13 17:44:40 +0000

    pygame

--- main.py
+++ main.py
@@ -1,71 +1,82 @@
 import random as r
 import time as t
 import sys as sus
-wooloo = r.randint(1,4)
+import pygame as game
+
+evolve = r.randint(1, 40)
+grooky_health = 50
+scorbunny_health = 50
 sobble_health = 50
 nicket_health = 40
 drednaw_health = 80
 pickachu_health = 35
 wooloo_health = 42
 eternatus_health = 140
-wooloo_attack = "tackle"
-wooloo_attack2 = "headbutt"
-wooloo_attack3 = "take down"
-wooloo_attack4 = "Double-Edge"
+
 while True:
-  starter = input("Which starter do you want Scorbunny, sobble, grooky ")
-  if starter == "sobble":
-    print("you chose sobble")
-    break
-  elif starter == "scorbunny":
-    print("you chose scorbunny")
-    break
-  elif starter == "grooky":
-    print("you chose grooky")
-    break
-  else:
-    print("that is not a starter choose another one")
+    starter = input("Which starter do you want Scorbunny, sobble, grooky ")
+    if starter == "sobble":
+        print("you chose sobble")
+        break
+    elif starter == "scorbunny":
+        print("you chose scorbunny")
+        break
+    elif starter == "grooky":
+        print("you chose grooky")
+        break
+    else:
+        print("that is not a starter choose another one")
 print("you head into the tall grass and you encountered a wild pokemon")
 t.sleep(4)
-x = r.randint(1,5)
+x = r.randint(1, 5)
 if x == 1:
     print("You encountered a wild pickachu")
 elif x == 2:
-  print("You encountered a wild wooloo")
+    print("You encountered a wild wooloo")
 elif x == 3:
-  print("You encountered a wild nickit")
+    print("You encountered a wild nickit")
 elif x == 4:
-  print("You encountered a tough looking drednaw")
+    print("You encountered a tough looking drednaw")
 elif x == 5:
-  print("You encountered a wild WOAH WHAT THE A WILD ETERNATUS")
+    print("You encountered a wild WOAH WHAT THE A WILD ETERNATUS")
 while x == 2:
-  answer = input("fight/catch/pokemon/run ")
-  if answer == "fight" and starter == "sobble" and x == 2:
-    print("what move do you want to use")
-    print("pound")
-    print("sucker punch")
-    print("water gun")
-    print("water pulse")
-    fight = input()
-    if fight == "water pulse":
-      print("that did 60 damage")
-      wooloo_health -= 60
-    if fight == "water gun":
-      print("that did 40 hp")
-      wooloo_health -= 40
-    elif wooloo == 2:
-      print("wooloo used tackle")
-      sobble_health -= 40
-    elif wooloo == 3:
-      print("wooloo used headbutt")
-      sobble_health -= 70
-    if sobble_health <= 0:
-      print("you have no pokemon that can fight")
-      sus.exit(':(')
-      break
-    if wooloo_health <= 0:
-      print("The wild wooloo fainted we'll get em next time")
-      break
-  if answer == "catch":
-    print("gotcha wooloo was caught!")
-    break
+    wooloo = r.randint(1, 4)
+    answer = input("fight/catch/pokemon/run ")
+    if answer == "fight" and starter == "sobble" and x == 2:
+        print("what move do you want to use")
+        print("pound")
+        print("sucker punch")
+        print("water gun")
+        print("water pulse")
+        fight = input()
+        if fight == "water pulse":
+            print("that did 60 damage")
+            wooloo_health -= 60
+        if fight == "water gun":
+            print("that did 40 hp")
+            wooloo_health -= 40
+        if wooloo == 4:
+            print("wooloo used Double-Edge")
+            sobble_health -= 120
+        elif wooloo == 1:
+            print("wooloo used take down")
+            sobble_health -= 90
+        elif wooloo == 2:
+            print("wooloo used tackle")
+            sobble_health -= 40
+        elif wooloo == 3:
+            print("wooloo used headbutt")
+            sobble_health -= 70
+        if sobble_health <= 0:
+            print("you have no pokemon that can fight")
+            sus.exit(':(')
+            break
+        if wooloo_health <= 0:
+            print("The wild wooloo fainted we'll get em next time")
+            break
+    if answer == "catch":
+        print("gotcha wooloo was caught!")
+        break
+    if evolve == 1:
+        print("what sobble is evolving")
+        print("congrats your sobble evovled to a drizzile")
